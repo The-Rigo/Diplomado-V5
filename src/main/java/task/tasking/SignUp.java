c77b93147094a8031fd4a6bc940ee48c4a2629a6
Rodrigo Ariel Ledezma Lopez <rodri.ledezma9483@gmail.com>
2023-12-08 19:58:08 -0400

    task

---
+++ src/main/java/task/tasking/SignUp.java
@@ -0,0 +1,20 @@
+package task.tasking;
+
+import actions.Click;
+import actions.Enter;
+import org.openqa.selenium.WebDriver;
+import ui.FormAuthentication;
+import ui.HomePage;
+import ui.HomePageUI;
+import ui.SignUpFree;
+
+public class SignUp {
+
+    public static void as(WebDriver webDriver, String fullname, String email, String pass) {
+        Click.on(webDriver, HomePage.signup);
+        Enter.text(webDriver, SignUpFree.fullname, fullname);
+        Enter.text(webDriver, SignUpFree.email, email);
+        Enter.text(webDriver, SignUpFree.password, pass);
+        Click.on(webDriver, SignUpFree.signupButton);
+    }
+}
--- src/main/java/ui/HomePage.java
+++ src/main/java/ui/HomePage.java
@@ -3,5 +3,5 @@ package ui;
 import org.openqa.selenium.By;
 
 public class HomePage {
-    public static By addRemove = By.xpath("//img[@src='/Images/design/pagesignup.png']");
+    public static By signup = By.xpath("//img[@src='/Images/design/pagesignup.png']");
 }
