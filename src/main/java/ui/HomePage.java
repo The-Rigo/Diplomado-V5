c0ef33f1c13c8ff83bcc9ca872032daadbb13cb1
Rodrigo Ariel Ledezma Lopez <rodri.ledezma9483@gmail.com>
2023-12-08 19:48:45 -0400

    first commit

---
+++ src/main/java/ui/HomePage.java
@@ -0,0 +1,7 @@
+package ui;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+    public static By addRemove = By.xpath("//img[@src='/Images/design/pagesignup.png']");
+}
---
+++ src/main/java/ui/SignUpFree.java
@@ -0,0 +1,11 @@
+package ui;
+
+import org.openqa.selenium.By;
+
+public class SignUpFree {
+    public static By fullname = By.id("ctl00_MainContent_SignupControl1_TextBoxFullName");
+    public static By email = By.id("ctl00_MainContent_SignupControl1_TextBoxEmail");
+    public static By password = By.id("ctl00_MainContent_SignupControl1_TextBoxPassword");
+
+    public static By signupButton = By.xpath("//div[@class='HPsignupDiv']//input[@class='HPLoginBtn']");
+}
